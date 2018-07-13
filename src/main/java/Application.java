ba507b6406080090dcb054196a21aab471044194
Minh Lee <minhlee.fre.mta@gmail.com>
2018-07-13 17:22:10 +0700

    run and calculated sum of characters in a string

---
+++ src/main/java/Application.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        String st;
+        System.out.println("Please enter a string");
+        Scanner input = new Scanner(System.in);
+        st = input.nextLine();
+
+        System.out.println("Your string has " + countLetter(st) + " characters!" );
+    }
+
+    private static int countLetter(String string) {
+        return string.length();
+    }
+}
